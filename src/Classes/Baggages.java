b37000208e90f77e59610b3cadde001de77fc698
student <student@moon>
2022-08-26 23:37:16 +0200

    FIX - added constructors

--- src/Classes/Baggages.java
+++ src/Classes/Baggages.java
@@ -6,6 +6,8 @@
 package Classes;
 
 import java.io.Serializable;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -17,21 +19,32 @@ public class Baggages implements Serializable
     public static int STATUS_NOT_LOADED = 0;
     public static int STATUS_REFUSED = 2;
     
-    public int ID, loaded;
+    public int ID, loadedState;
     public float weight;
     public String IdBillet,remarks;
     public boolean IsValise, isVerified,isReceived;
     
-    public Baggages (int ID, String idbillet, float poids,boolean isvalise)
+    public Baggages (int ID, String idbillet, float poids,boolean isvalise,boolean isreceived, int loadstate, boolean isverified, String remark)
     {
         this.ID = ID;
         this.IdBillet = idbillet;
         this.weight = poids;
         this.IsValise= isvalise;
-        this.loaded = 0;
-        this.isReceived = false;
-        this.isVerified = false;
-        this.remarks = "";
+        this.isReceived = isreceived;
+        this.loadedState = loadstate;
+        this.isVerified = isverified;
+        this.remarks = remark;
+    }
+    
+    public Baggages (ResultSet rs) throws SQLException{
+        this.ID             = rs.getInt(1);
+        this.IdBillet       = rs.getString(2);
+        this.weight         = rs.getFloat(3);
+        this.IsValise       = rs.getBoolean(4);
+        this.isReceived     = rs.getBoolean(5);
+        this.loadedState    = rs.getInt(6);
+        this.isVerified     = rs.getBoolean(7);
+        this.remarks        = rs.getString(8);
     }
     
     public String AfficheBaggages()
